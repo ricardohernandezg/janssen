98e5e111acc3f561faafd8bdb9aef4c3167f0c82
Ricardo Hernández <ricardohernandezg@gmail.com>
2025-09-25 17:39:42 -0400

    reingenieering events

--- src/Engine/Event.php
+++ src/Engine/Event.php
@@ -38,7 +38,6 @@ abstract class Event
      */
     abstract function handle($args);
 
-
     /**
      * Ask config for all events and run the correspondent if found
      *
@@ -50,33 +49,10 @@ abstract class Event
      */
     public static function invoke($event, ...$args)
     {
-        /*
-        $events = Config::getEvents();
-        if(empty($events))
-            return $args;*/
 
         if(!self::exists($event)){
             throw new Exception("This event is not available");
         }        
-
-        /*
-        $a = explode('.',$event_name, 2); // name shouldn't have more than 2 members class.event
-        if(is_array($a) && !empty($a[0]) && !empty($a[1]))
-            $event_path = $a[0] . "\\" . $a[1] . 'Event';
-        else
-            throw new Exception("Invalid Event name to invoke");
-        
-        $class = "\\App\\Event\\" . $event_path;
-        if(class_exists($class)){
-            $i = new $class;
-            if(method_exists($i, 'handle'))
-                return call_user_func_array([$i, "handle"], $args);
-            else
-                throw new Exception("Class Event neeed the handle() event to be defined");   
-        }
-        
-        return $args;
-        */
 
         foreach(self::$callables[$event] as $k=>$callable){
             $callable($args);
