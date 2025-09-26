45f74ec05f809828991234005b949bb602297cd9
Ricardo Hernández <ricardohernandezg@gmail.com>
2025-09-25 22:13:02 -0400

    information queries for PostgresAdaptor

--- src/Helpers/Database/Adaptors/PostgresAdaptor.php
+++ src/Helpers/Database/Adaptors/PostgresAdaptor.php
@@ -36,13 +36,35 @@ class PostgresAdaptor extends Adaptor
         // RETURNING id field statement to the query
     }
 
-    public function tableExists($name, $schema = null){}
+    public function tableExists($table_name, $schema = null){
+        $sql = "SELECT tablename 
+        FROM pg_catalog.pg_tables 
+        WHERE schemaname = '$schema' 
+        AND tablename = '$table_name';";
+    }
 
-    public function viewExists($name, $schema = null){}
+    public function viewExists($view_name, $schema = null){
+        $sql = "SELECT table_name 
+        FROM information_schema.views 
+        WHERE table_schema = '$schema' 
+        AND table_name = '$view_name';";        
+    }
 
-    public function procedureExists($name, $schema = null){}
+    public function procedureExists($procedure_name, $schema = null){
+        $sql = "SELECT routine_name 
+        FROM information_schema.routines 
+        WHERE specific_schema = '$schema' 
+        AND routine_type = 'PROCEDURE' 
+        AND routine_name = '$procedure_name';";
+    }
     
-    public function functionExists($name, $schema = null){} 
+    public function functionExists($function_name, $schema = null){
+        $sql = "SELECT routine_name 
+        FROM information_schema.routines 
+        WHERE specific_schema = '$schema' 
+        AND routine_type = 'FUNCTION' 
+        AND routine_name = '$function_name';";
+    } 
 
     /**
      * Try to find the default schema from configuration
